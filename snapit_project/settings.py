095c34be734bd560bbe445786842473865eabf9f
kingkongjaat <Chaudharyajay9896@gmail.com>
2026-04-26 19:51:35 +0530

    Update settings.py

--- snapit_project/settings.py
+++ snapit_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+(5#umllcz=2)_lg(kju0fuifk7&=@9ulgy!^5+qve1$psf-hh')
+SECRET_KEY = os.getenv('SECRET_KEY', "YOUR_DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
